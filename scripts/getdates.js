9aeacb418457f4efae1f8f58376f55f269c8f375
lcauffman <104706384+lcauffman@users.noreply.github.com>
2024-01-20 15:12:20 -0700

    Done

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,2 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var lastModified= document.lastModified;
-
-    var options = { year: 'numeric', month: 'long', day: 'numeric'};
-
-    var formattedDate = newDate(lastModified).toLocaleDateString('en-US', options);
-    document.getElementById("lastModified").textContent = "Last Modified: " + formattedDate;
-});
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
