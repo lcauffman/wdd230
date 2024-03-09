d93f156e5a8d416c7cdd821a350bc5176abc13f2
lcauffman <104706384+lcauffman@users.noreply.github.com>
2024-03-09 13:48:24 -0700

    updates

---
+++ lesson07/scripts/script1.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value == '') {
+        alert("Enter a Value");
+    } else {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+            input.value = "";
+        });
+    }
+});
